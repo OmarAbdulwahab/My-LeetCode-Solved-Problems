e3f44df5a0016826959c37fcec50bb79e06bd2ac
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-04-26 04:26:39 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (5.34%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -6,7 +6,8 @@ public:
         for(int i=0;i<s.length();i++){
             if(s[i]=='(')st.push(s[i]);
             else if(s[i]==')'){
-                if(st.size()>ans)ans=st.size();
+                if(st.size()>ans)
+                    ans=st.size();
                 st.pop();
             }
         }

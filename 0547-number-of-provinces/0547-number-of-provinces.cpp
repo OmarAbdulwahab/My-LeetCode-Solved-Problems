f8b7fed802cfda4e86ea0a5732f086b16da769a2
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-06-19 18:58:01 +0300

    Time: 23 ms (85.51%), Space: 15.3 MB (12.97%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int size = isConnected.size();
+        vector<bool>vis(size, false);
+        int cnt = 0;
+        for(int cur = 0; cur<size; cur++){
+            if(!vis[cur]){
+                cnt++;
+                traverseGroup(cur, isConnected, vis, size);
+            }
+        }
+        
+        return cnt;
+    }
+    
+private:
+    void traverseGroup(int cur, vector<vector<int>>& isConnected, vector<bool>& vis, int size){
+        vis[cur] = true;
+        for(int nxt=0; nxt<size; nxt++){
+            if(isConnected[cur][nxt] && !vis[nxt])
+                traverseGroup(nxt, isConnected, vis, size);
+        }
+    }
+};

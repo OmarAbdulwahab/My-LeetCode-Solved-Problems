a5774b66984e0e9f3197d2f308b2799eb7983a71
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-02 18:18:23 +0300

    Time: 0 ms (100.00%), Space: 12.1 MB (22.63%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // The trickest part of this problem is to figure out that the solution is using STACK and the rest is easy
     int evalRPN(vector<string>& tokens) {
         stack<int>st;
         for(auto el:tokens){
@@ -12,7 +13,8 @@ public:
             st.pop();
             int tmp2=st.top();
             st.pop();
-            int newEl = el=="+"?tmp+tmp2:el=="-"?tmp2-tmp:el=="*"?tmp*tmp2:tmp2/tmp;
+            
+            int newEl = el=="+"?tmp+tmp2: el=="-"?tmp2-tmp: el=="*"?tmp*tmp2: tmp2/tmp;
             st.push(newEl);
         }
         

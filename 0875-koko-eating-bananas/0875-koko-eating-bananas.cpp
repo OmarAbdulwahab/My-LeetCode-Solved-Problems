fee75e6f866c5a14e9e6076eee58d5103fdf4e62
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-05 17:45:22 +0300

    Time: 39 ms (65.36%), Space: 19.2 MB (12.57%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -7,8 +7,6 @@ public:
             int k = l + (r-l)/2;
             long hours = 0;
             for(int i = 0; i<piles.size();i++){
-                // double t = piles[i]/k;
-                // hours += ceil(t);
                 hours += ceil((double) piles[i] / k);
             }
             if(hours<=h){
@@ -24,25 +22,3 @@ public:
     }
 };
 
-/*
-int l = 1, r = *max_element(piles.begin(),piles.end());
-        int ans = r;
-        while(l<=r){
-            int k = l + (r-l)/2;
-            long hours = 0;
-            for(int i = 0; i<piles.size();i++){
-                double t = piles[i]/k;
-                hours += ceil(t);
-                hours += ceil((double) piles[i] / k);
-            }
-            if(hours<=h){
-                ans = min(ans,k);
-                r = k-1;
-            }
-            else{
-                l = k+1;
-            }
-        }
-        
-        return ans;
-*/

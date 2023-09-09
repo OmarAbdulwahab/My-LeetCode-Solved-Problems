a0a1501a070488a56d42ef0626c35f3c83439ad8
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-09 18:30:33 +0300

    Time: 0 ms (100.00%), Space: 9 MB (96.96%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
+        // All the elements in the array should be counted except for the minimum and maximum elements.
         int n = nums.size();
         if(count(nums.begin(),nums.end(),nums[0])==n)return 0;
         

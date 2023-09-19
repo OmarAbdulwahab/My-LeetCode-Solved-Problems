16462400904ac9d51bd84301c6344ac97d9641f2
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-19 18:45:12 +0300

    Time: 262 ms (5.68%), Space: 85.9 MB (11.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,8 +3,8 @@ public:
     int findDuplicate(vector<int>& nums) {
         map<int,int>mp;
         for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]])return nums[i];
             mp[nums[i]]++;
-            if(mp[nums[i]]>1)return nums[i];
         }
     
     return 0;

16d8fcc3c1596ea96bfc1573103019f462a4bf55
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-19 22:05:23 +0300

    Time: 2081 ms (7.45%), Space: 6.4 MB (7.05%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -11,3 +11,6 @@ public:
         return ans;
     }
 };
+
+// Solution:
+// https://leetcode.com/problems/unique-binary-search-trees/discuss/1565543/C%2B%2BPython-5-Easy-Solutions-w-Explanation-or-Optimization-from-Brute-Force-to-DP-to-Catalan-O(N)

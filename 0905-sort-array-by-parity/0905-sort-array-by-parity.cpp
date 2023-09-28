ef7c9fb9fc715e06ea2f81a818739e2e8f40aa0b
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-09-28 06:43:11 +0300

    Time: 12 ms (18.97%), Space: 17.8 MB (6.46%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int>even,odd;
-        for(auto it:nums)
-            it&1?odd.push_back(it):even.push_back(it);
+//         vector<int>even,odd;
+//         for(auto it:nums)
+//             it&1?odd.push_back(it):even.push_back(it);
+        
+//         vector<int>ans;
+//         for(auto it:even)ans.push_back(it);
+//         for(auto it:odd)ans.push_back(it);
+        
+        deque<int>dq;
+        for(int num:nums)
+            num&1?dq.push_back(num):dq.push_front(num);
         
         vector<int>ans;
-        for(auto it:even)ans.push_back(it);
-        for(auto it:odd)ans.push_back(it);
-        
+        while(!dq.empty()){
+            ans.push_back(dq.front());
+            dq.pop_front();
+        }
         
         return ans;
     }

c118b62efd897e4e9cf3950b754e12530b4c846f
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2023-12-23 17:11:40 +0200

    Time: 5 ms (19.89%), Space: 7.2 MB (64.64%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    bool isPathCrossing(string path) {
+        unordered_map<char, pair<int, int>> moves;
+        moves['N'] = {0, 1};
+        moves['S'] = {0, -1};
+        moves['W'] = {-1, 0};
+        moves['E'] = {1, 0}; 
+        
+        set<string>st;
+        st.insert("0,0");
+        
+        int x=0,y=0;
+        
+        for(char ch:path){
+            pair<int,int> currMove = moves[ch];
+            int dx = currMove.first;
+            int dy = currMove.second;
+            
+            x+=dx;
+            y+=dy;
+            
+            string newPoint = to_string(x)+","+to_string(y);
+            
+            if(st.find(newPoint)!=st.end())return true;
+            st.insert(newPoint);
+        }
+        
+        
+        return false;
+    }
+private:
+//     pair<int,int> newPoint(char ch){
+//         if(ch=='N') j++;
+//         else if(ch=='S') j--;
+//         else if(ch=='E')i++;
+//         else i--;
+        
+//         return {i,j};
+//     }
+};

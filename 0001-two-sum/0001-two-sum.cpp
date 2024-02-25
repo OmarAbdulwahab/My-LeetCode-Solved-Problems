b7aaf29747b3696d9bbb2d65c20ebbd937e51cb3
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-02-25 02:03:22 +0200

    Time: 48 ms (45.16%), Space: 12.7 MB (33.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>answer;
+        // vector<int>answer;
         //sort(nums.begin(),nums.end());
         //int p1=0,p2=nums.size()-1;
         int n=nums.size();
@@ -9,14 +9,10 @@ public:
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if((nums[i]+nums[j])==target){
-                    answer.push_back(i);
-                    answer.push_back(j);
-                    break;
+                    return {i,j};
                 }
             }
         }
-        //answer.push_back(p1);
-        //answer.push_back(p2);
-        return answer;
+        return {};
     }
 };

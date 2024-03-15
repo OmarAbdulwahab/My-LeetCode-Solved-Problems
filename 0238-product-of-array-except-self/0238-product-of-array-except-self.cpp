12f2231e70621be34836c0a60271d0b7f6b02d13
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-03-15 15:09:01 +0200

    Time: 11 ms (90.04%), Space: 26.4 MB (55.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prod=1,cntZero=count(nums.begin(),nums.end(),0);
-        if(cntZero>1)return vector<int>(nums.size());
+        int prod=1, cntZero=count(nums.begin(),nums.end(),0);
+        if(cntZero>1)
+            return vector<int>(nums.size());
+        
         for(auto el:nums)
             if(el)prod*=el;
         

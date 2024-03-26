5b23dcee81641c212196ce1d1c9e943fb6955942
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-03-26 14:48:24 +0200

    Time: 42 ms (67.55%), Space: 43.6 MB (81.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -11,7 +11,7 @@ public:
         for(int i=0;i<n;i++){
             while(nums[i]>0 and nums[i]<=n and nums[nums[i]-1]!=nums[i])
                 swap(nums[i],nums[nums[i]-1]);
-        }
+        } 
         
         for(int i=0;i<n;i++)
             if(nums[i]!=i+1)return i+1;

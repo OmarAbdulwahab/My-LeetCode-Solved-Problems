173ac227b34f4dc8b3b1bfa560eae20011dc2471
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-03-30 00:21:54 +0200

    Time: 845 ms (91.67%), Space: 30.9 MB (79.76%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -7,12 +7,18 @@ class Solution:
         for r in range(len(nums)):
             if nums[r] == mx:
                 cnt += 1
-            while cnt > k or (cnt == k and nums[l] != mx and l<=r):
+            # this is another way to write it
+            while cnt == k:
                 if nums[l] == mx:
                     cnt -= 1
                 l += 1
+            ans += l
+#             while cnt > k or (cnt == k and nums[l] != mx and l<=r):
+#                 if nums[l] == mx:
+#                     cnt -= 1
+#                 l += 1
             
-            if cnt == k:
-                ans += l+1
+#             if cnt == k:
+#                 ans += l+1
                 
         return ans

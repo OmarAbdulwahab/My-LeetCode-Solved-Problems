2c7eb552fde97e1306273e0fb1b688d45d17b9e1
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-04-05 05:09:05 +0200

    Time: 0 ms (100.00%), Space: 8.1 MB (34.23%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,39 +1,9 @@
 class Solution {
 public:
     string makeGood(string s) {
-   //     while(s.size()>1){
-   //     bool find=false;
-   //     for(int i=0;i<s.size()-1;i++){
-   //         if(s[i]==(s[i+1]^0x20)){
-   //             s=s.substr(0,i)+s.substr(i+2);
-   //             find=true;
-   //             break;
-   //         }
-   //     }
-   //     if(!find)break;
-   // }
-   // return s;
         
         stack<char>stk;
-        // for(char c:s){
-        //     if(!stk.empty()){
-        //         bool small = c-'a'>=0;
-        //         if(small){
-        //             if(stk.top()-'a' < 0){
-        //                 if(stk.top()-c+20 == 0)stk.pop();
-        //                 else stk.push(c);
-        //             }
-        //             else stk.push(c);
-        //         }
-        //         else{
-        //           if(c-stk.top()+20 == 0)stk.pop(); 
-        //           else stk.push(c); 
-        //         } 
-        //     }
-        //     else{
-        //         stk.push(c);
-        //     }
-        // }
+        
         for(char c:s){
             if(!stk.empty() and stk.top()!=c and lower(stk.top())==lower(c))
                 stk.pop();
@@ -49,6 +19,7 @@ public:
         reverse(ans.begin(), ans.end());
         return ans;
     }
+    
 private:
     char lower(char c){
         if(c<'a'){

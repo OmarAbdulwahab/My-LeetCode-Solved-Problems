f11cfa8d36f4282ef2bbeb3617b08c35677b3058
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-04-08 03:33:19 +0200

    Time: 0 ms (100.00%), Space: 10.9 MB (50.25%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        // int count[] = {0, 0}, n = students.size(), k=0;
-        // for (int a: students)
-        //     count[a]++;
-        // for (; k < n && count[sandwiches[k]] > 0; ++k)
-        //     count[sandwiches[k]]--;
-        // return n - k;
         int j=0;
         int z=0,one=0;
-        for(int n:students)n==0?z++:one++;
+        for(int n:students)
+            n==0?z++:one++;
         
         while(j<sandwiches.size()){
             if(sandwiches[j++]==0)z--;

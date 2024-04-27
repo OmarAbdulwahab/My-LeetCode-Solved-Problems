75f8f34a6198e0865516a1a210b32160afcd94a4
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-04-27 17:41:19 +0300

    Time: 2445 ms (6.27%), Space: 16.6 MB (76.47%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -1,5 +1,26 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
+        # neetcode dp buttom-up solution
+        dp = [0] * len(ring)
+        
+        for k in reversed(range(len(key))):
+            next_dp = [float("inf")] * len(ring)
+            for r in range(len(ring)):
+                for i, c in enumerate(ring):
+                    if c == key[k]:
+                        min_dist = min(
+                            abs(r-i), # between
+                            len(ring) - abs(r-i) # around
+                        )
+
+                        next_dp[r] = min(next_dp[r], min_dist + 1 + dp[i])
+            dp = next_dp
+        
+        return dp[0]
+        
+        
+        
+        '''
         # neetcode recursive solution
         cache = {}
         
@@ -24,4 +45,5 @@ class Solution:
             return res
         
         return helper(0, 0)
+        '''
                     

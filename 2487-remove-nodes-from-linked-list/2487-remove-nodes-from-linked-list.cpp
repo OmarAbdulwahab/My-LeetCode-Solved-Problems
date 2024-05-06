71f4f3f1e1bb2749da78d581abffc0d4367f7477
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-05-07 02:46:24 +0300

    Time: 254 ms (86.95%), Space: 159.7 MB (97.15%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -11,49 +11,81 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        if(head->next == NULL)return head;
-        // keep the correct nodes in a monotonic stack
-        stack<int> st;
-        ListNode* node = head;
-        while(node != NULL){
-            while(!st.empty() and st.top() < node->val)
-                st.pop();
-            
-            st.push(node->val);
-            node = node->next;
+        if (head->next == NULL)
+            return head;
+        
+        ListNode* new_head = reverse(head);
+        ListNode* cur = new_head;
+        int mx = cur->val;
+        while(cur->next != NULL){
+            // ListNode* prev = cur;
+            if(cur->next->val < mx){
+                cur->next = cur->next->next;
+                // cur->next = tmp;
+            }
+            else{
+                mx = cur->next->val;
+                cur = cur->next;
+            }
+            // prev = cur;
+            // mx = cur->next->val;
         }
         
+        return reverse(new_head);
+        
+        
+        
+        // --------------------------------------------------------------------------------------------
+        // keep the correct nodes in a monotonic stack (decreasing)
+        // input: [24,5,2,13,3,8]
+//         stack<int> st;
+//         ListNode* node = head;
+//         while (node != NULL) {
+//             while (!st.empty() and st.top() < node->val)
+//                 st.pop();
+
+//             st.push(node->val);
+//             node = node->next;
+//         }
+        // output: [24, 13, 8]
+
         // convert the stack into a linked list
-        ListNode* dummy = new ListNode();
-        node = dummy;
-        while(!st.empty()){
-            int value = st.top();
-            ListNode* tmp = new ListNode(value);
-            node->next = tmp;
-            node = node->next;
-            st.pop();
+        // ListNode* dummy = new ListNode();
+        // node = dummy;
+        // while (!st.empty()) {
+        //     int value = st.top();
+        //     ListNode* tmp = new ListNode(value);
+        //     node->next = tmp;
+        //     node = node->next;
+        //     st.pop();
+        // }
+        // output: 8 -> 13 -> 24
+
+        // reverse the linked list
+        // node = dummy->next;
+        // ListNode* tmp = node->next;
+        // node->next = NULL;
+        // while (tmp != NULL) {
+        //     ListNode* nxt = tmp->next;
+        //     tmp->next = node;
+        //     node = tmp;
+        //     tmp = nxt;
+        // }
+        // output: 24 -> 13 -> 8
+
+        // return reverse(dummy->next);
+    }
+private:
+    ListNode* reverse(ListNode* node){
+        ListNode* prev = NULL;
+        ListNode* cur = node;
+        while(cur != NULL){
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
         }
         
-        // reverse the linked list 
-        node = dummy->next;
-        ListNode* tmp = node->next;
-        // ListNode* nxt = tmp->next;
-        node->next = NULL;
-        while(tmp != NULL){
-            ListNode* nxt = tmp->next;
-            tmp->next = node;
-            node = tmp;
-            tmp = nxt;
-        }
-        
-        return node;
+        return prev;
     }
 };
-
-
-
-
-
-
-
-

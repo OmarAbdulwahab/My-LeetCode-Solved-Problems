e1df7a601744c6c9b12e123a4fb0e3c811885322
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-07-09 15:19:42 +0300

    Time: 144 ms (74.12%), Space: 76.5 MB (83.82%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
+        // time: O(n), space:O(1)
+        // Si = customers[i][0]
+        // Ei = customers[i][1] 
         double total = customers[0][1];
         int S = customers[0][0] + customers[0][1];
         
         for(int i=1; i<customers.size();i++){
-            if(customers[i][0] > S){
-                total += customers[i][1];
-                S = customers[i][0] + customers[i][1];
+            int Si = customers[i][0];
+            int Ei = customers[i][1];
+            if(Si > S){
+                total += Ei;
+                S = Si + Ei;
             }
             else{
-                total += (S + customers[i][1] - customers[i][0]);
-                S += customers[i][1];
+                total += (S + Ei - Si);
+                S += Ei;
             }
         }
         

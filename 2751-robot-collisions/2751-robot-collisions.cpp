f361dd07c8ff90fd063e73caa166ea01eafadce0
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-07-13 17:54:46 +0300

    Time: 613 ms (7.63%), Space: 267.7 MB (15.65%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,68 +1,47 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        // My solution ;) after 2 hours
-        int n = positions.size();
-        // robots: {position, {{number, health}, direction}}
-        vector<vector<int>> robots(n, vector<int>(4));
-        // robots[i][0] = position
-        // robots[i][1] = number
-        // robots[i][2] = health
-        // robots[i][3] = direction
+        // my solution
         map<int,int>mp;
-        // map: number -> health
-        for(int i=0; i<n; i++){
-            robots[i][0] = positions[i];
-            robots[i][1] = i+1;
-            robots[i][2] = healths[i];
-            robots[i][3] = directions[i];
-            
-            mp[i+1] = healths[i];
+        // map: position -> indx
+        for(int i=0; i<positions.size(); i++){
+            mp[positions[i]] = i;
         }
-        sort(robots.begin(), robots.end());
         
-        stack<vector<int>>stk;
-        for(int i=0; i<n ; i++){
-            if(stk.empty()){
-                stk.push(robots[i]);
-                continue;
-            }
+        stack<int>stk; // storing positions
+        for(auto it: mp){
+            // it.first = stk.top() = position
+            // it.second = mp[stk.top()] = indx
             
-            while(!stk.empty() and  stk.top()[3] == 'R' and robots[i][3] == 'L'){
-                int curNumber = robots[i][1];
-                int curHealth = robots[i][2];
-
-                int topNumber = stk.top()[1];
-                int topHealth = stk.top()[2];
+            while(!stk.empty() and  directions[mp[stk.top()]] == 'R' and directions[it.second] == 'L'){
+                int curHealth = healths[it.second];
+                int topHealth = healths[mp[stk.top()]];
                 
                 if(curHealth > topHealth){ // cur > top
-                    mp[curNumber]--;
-                    robots[i][2] -= 1; // curHealth -= 1
-                    topHealth = 0;
-                    mp[topNumber] = 0;
+                    healths[it.second]--;
+                    healths[mp[stk.top()]] = 0;
                     stk.pop();
                 }else if(curHealth < topHealth){ // cur < top
-                    mp[topNumber]--;
-                    stk.top()[2] -= 1; // topHealth -= 1
-                    curHealth=0;
-                    robots[i][2] = 0;
-                    mp[curNumber] = 0;
+                    healths[mp[stk.top()]]--;
+                    healths[it.second] = 0;
                     break;
                 }else{ // cur == top
-                    mp[curNumber] = 0;
-                    mp[topNumber] = 0;
-                    robots[i][2] = 0;
-                    stk.top()[2] = 0;
+                    healths[it.second] = 0;
+                    healths[mp[stk.top()]] = 0;
                     stk.pop();
                     break;
                 }
             }
-            if(robots[i][2]>0)
-                stk.push(robots[i]);
+            if(healths[it.second] > 0)
+                stk.push(it.first);
         }
         
         vector<int> ans;
+        map<int,int>ansMap; // map: original order -> health
         for(auto it:mp){
+            ansMap[it.second] = healths[it.second];
+        }
+        for(auto it:ansMap){
             if(it.second != 0)
                 ans.push_back(it.second);
         }

3fbfc871d7855e23700524d30a9d26ba516b8ce1
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-07-19 16:32:18 +0300

    Time: 16 ms (56.65%), Space: 14.6 MB (12.50%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        set<int>mxset;
+        set<int>mxset; // find max in each column
         for(int j=0;j<m;j++){
             int mx = 0;
             for(int i=0; i<n; i++){
@@ -12,7 +12,7 @@ public:
             mxset.insert(mx);
         }
         
-        vector<int>ans;
+        vector<int>ans; // if a min in a row is also in mxset so it is a valid answer
         for(int i=0; i<matrix.size();i++){
             int mn = *min_element(matrix[i].begin(), matrix[i].end());
             if(mxset.find(mn) != mxset.end())

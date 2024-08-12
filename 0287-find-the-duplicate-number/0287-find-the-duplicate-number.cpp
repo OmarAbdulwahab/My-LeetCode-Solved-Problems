c170667f20b123479f449a939f46b873f6025aa6
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-08-12 22:40:34 +0300

    Time: 227 ms (5.07%), Space: 89.5 MB (9.82%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    // neetcode
     int findDuplicate(vector<int>& nums) {
-        // hashmap solution is O(n) time and space (it fits btw but its not the approach we want)
-        // to solve it in time: O(n) and space: O(1)
-        // You have to deal with it as a linked list and to know Floyd's algorithm
-        
-        int slow=0, fast=0;
-        while(true){
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            if(slow==fast)break;
+        map<int,int>mp;
+        for(int n:nums){
+            if(mp[n])return n;
+            mp[n]++;
         }
         
-        int slow2 = 0;
-        while(true){
-            slow = nums[slow];
-            slow2 = nums[slow2];
-            if(slow == slow2)break;
-        }
-        
-        return slow;
+        return 0;
     }
 };

26b8711d24298040a58110f956fe4d1f4dcd83fa
Omar Abdulwahab <113452318+OmarAbdulwahab@users.noreply.github.com>
2024-09-25 18:24:45 +0300

    Time: 3610 ms (11.73%), Space: 598.3 MB (10.35%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -2,7 +2,7 @@ class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         ans = []
         count = defaultdict(int)
-        
+        # O(n * l * l)
         for w in words:
             for i in range(len(w)):
                 count[w[:i+1]] += 1
